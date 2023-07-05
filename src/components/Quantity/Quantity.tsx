bbc7fe01051d795ac12bcaa0a1a461d8b2673180
Vedar <islamparchev006@gmail.com>
2023-07-05 22:58:27 +0300

    slider-navigation-arrows

--- src/components/Quantity/Quantity.tsx
+++ src/components/Quantity/Quantity.tsx
@@ -1,18 +1,58 @@
 import React,{FC} from 'react'
 import styles from "./Quantity.module.scss";
 import {Swiper,SwiperSlide} from 'swiper/react';
+import { Navigation, Pagination, A11y } from 'swiper/modules';
+
 import 'swiper/css';
+import SliderNavigation from '../SliderNavigation/SliderNavigation';
 const Quantity:FC = () => {
   return (
     <section className={`${styles.quantity}`}>
-        <div className={`container ${styles.quantity__container}`}>
-      <Swiper>
-        <SwiperSlide>1111111</SwiperSlide>
-        <SwiperSlide>2222222</SwiperSlide>
-        <SwiperSlide>3333333</SwiperSlide>
+       
+      <Swiper
+      // @ts-ignore
+      modules={[Pagination,Navigation,A11y]}
+      pagination={{clickable:true}}
+      className='Quantity__slider'>
+        <SwiperSlide>
+          <div className={`${styles.quantity__slide}`}>
+           <div className={`${styles.quantity__slide_text}`}>
+           <h1 className={`${styles.quantity__title}`}>
+            Исключительное качество без компромиссов
+            </h1>
+            <div className={`${styles.quantity__descr}`}>
+            Ножи «Tuotown» – это главный инструмент поваров и секрет кулинарного мастерства
+            </div>
+           </div>
+            <button className={`btn-reset ${styles.quantity__btn}`}>ПОДРОБНЕЕ</button>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={`${styles.quantity__slide}`}>
+            <h1 className={`${styles.quantity__title}`}>
+            Исключительное качество без компромиссов
+            </h1>
+            <div className={`${styles.quantity__descr}`}>
+            Условие, которое необходимо сделать, чтобы они были уничтожены.
+            </div>
+            <button className={`btn-reset ${styles.quantity__btn}`}>ПОДРОБНЕЕ</button>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={`${styles.quantity__slide}`}>
+            <h1 className={`${styles.quantity__title}`}>
+            Исключительное качество без компромиссов
+            </h1>
+            <div className={`${styles.quantity__descr}`}>
+            Условие, которое необходимо сделать, чтобы они были уничтожены.
+            </div>
+            <button className={`btn-reset ${styles.quantity__btn}`}>ПОДРОБНЕЕ</button>
+          </div>
+        </SwiperSlide>
+        <SliderNavigation/>
       </Swiper>
       
-        </div>
+        
     </section>
   )
 }
