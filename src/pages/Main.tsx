0d3f95ff6c55149f55f14bfc73a453e4b0ca8550
Vedar <islamparchev006@gmail.com>
2023-07-08 22:52:02 +0300

    aboutUs-start

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,12 +1,14 @@
 import React,{FC} from 'react'
 import Quantity from '../components/Quantity/Quantity'
 import Categories from '../components/Categories/Categories'
+import AboutUs from '../components/AboutUs/AboutUs'
 
 const Main:FC = () => {
   return (
     <main className='Main'>
       <Quantity/>
       <Categories/>
+      <AboutUs/>
     </main>
   )
 }
