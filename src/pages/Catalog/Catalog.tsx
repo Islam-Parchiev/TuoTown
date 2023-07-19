86787e0d842a6e6882c4e44086f1fe6842a46133
Vedar <islamparchev006@gmail.com>
2023-07-19 19:01:30 +0300

    create-accordion

--- src/pages/Catalog/Catalog.tsx
+++ src/pages/Catalog/Catalog.tsx
@@ -2,12 +2,19 @@ import React from 'react'
 
 import Sidebar from '../../components/Sidebar/Sidebar'
 import Home from '../../components/Home/Home'
+import Filter from '../../components/Filter/Filter'
 
 const Catalog:React.FC<any> = ({toggleSidebar,setToggleSidebar}) => {
 	return (
 		<main className="Main">
 			{toggleSidebar === true ? <Sidebar toggleSidebar={toggleSidebar} setToggleSidebar={setToggleSidebar}/>:null}
 			<Home/>
+			<div className="container">
+				<div className="Main__wrapper">
+					<Filter/>
+				</div>
+			</div>
+			
 		</main>
 	)
 }
