05cc19e2c4877147dd8ab7ba59790c0a296cc66f
Vedar <islamparchev006@gmail.com>
2023-07-21 09:54:34 +0300

    Basket__form-html-start

--- src/pages/Basket/Basket.tsx
+++ src/pages/Basket/Basket.tsx
@@ -54,7 +54,67 @@ const Basket:React.FC<any> = ({toggleSidebar,setToggleSidebar}) => {
 						</div>
 					</div>
 				</div>
-				
+				<ul className="list-reset Basket__services">
+                   	<li className="Basket__services-item services__item">
+                      	<div className="services__item-img">
+                          	<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36" fill="none">
+								<path d="M30 15.2727C30 24.8182 18 33 18 33C18 33 6 24.8182 6 15.2727C6 12.0178 
+								7.26428 8.89618 9.51472 6.5946C11.7652 4.29302 14.8174 3 18 3C21.1826 3 24.2348
+								 4.29302 26.4853 6.5946C28.7357 8.89618 30 12.0178 30 15.2727Z" stroke="white" 
+								 stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round"/>
+								<path d="M18 19.3636C20.2091 19.3636 22 17.5321 22 15.2727C22 13.0134 20.2091 
+								11.1818 18 11.1818C15.7909 11.1818 14 13.0134 14 15.2727C14 17.5321 15.7909 
+								19.3636 18 19.3636Z" stroke="white" stroke-width="1.3" stroke-linecap="round" 
+								stroke-linejoin="round"/>
+							</svg>
+
+					  	</div>
+						<div className="services__item-text">
+							<h4>Доставка</h4>
+							<span>Саратов, 25.10 бесплатно</span>
+						</div>
+				   </li>
+				   <li className="Basket__services-item services__item">
+				   <div className="services__item-img">
+                          	<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36" fill="none">
+								<path d="M30 15.2727C30 24.8182 18 33 18 33C18 33 6 24.8182 6 15.2727C6 12.0178 
+								7.26428 8.89618 9.51472 6.5946C11.7652 4.29302 14.8174 3 18 3C21.1826 3 24.2348
+								 4.29302 26.4853 6.5946C28.7357 8.89618 30 12.0178 30 15.2727Z" stroke="white" 
+								 stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round"/>
+								<path d="M18 19.3636C20.2091 19.3636 22 17.5321 22 15.2727C22 13.0134 20.2091 
+								11.1818 18 11.1818C15.7909 11.1818 14 13.0134 14 15.2727C14 17.5321 15.7909 
+								19.3636 18 19.3636Z" stroke="white" stroke-width="1.3" stroke-linecap="round" 
+								stroke-linejoin="round"/>
+							</svg>
+
+					  	</div>
+						<div className="services__item-text">
+							<h4>Доставка</h4>
+							<span>Саратов, 25.10 бесплатно</span>
+						</div>
+				   </li>
+				</ul>
+				<div className="Basket__form">
+					<h2 className="Basket__form-title">
+					Для подтверждения заказа - введите ваши данные и мы перезвоним вам
+					</h2>
+					<form action="/" className="Basket__form-form">
+						<label className="Basket__form-label">
+							<span>Получатель</span>
+							<input className="input-reset" type="text" placeholder="Имя Фамилия" />
+						</label>
+						<label className="Basket__form-label">
+							<span>Мобильный телефон</span>
+							<input className="input-reset" type="text" placeholder="+7 (___) ___-__-__" />
+						</label>
+						<label className="Basket__form-label">
+							<span>E-mail</span>
+							<input className="input-reset" type="text" placeholder="Ваша почта" />
+						</label>
+						
+						
+					</form>
+				</div>
 			</div>
 		</main>
 	)
