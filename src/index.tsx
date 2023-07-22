97a5e40493a1b930552833adeb426414e23bd1f7
Vedar <islamparchev006@gmail.com>
2023-07-22 22:14:17 +0300

    add-reduxToolkit

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter  } from 'react-router-dom';
+
+
+import { Provider } from 'react-redux';
+
+import { store } from './redux/store';
+
 
 import './index.scss';
 import App from './App';
@@ -11,9 +17,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
 	<React.StrictMode>
-		<Router>
-			<App />
-		</Router>
+		<BrowserRouter>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</BrowserRouter>
 	</React.StrictMode>,
 );
 
