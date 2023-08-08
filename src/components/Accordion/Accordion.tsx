b1f69202080ff028a511620437ea0bfa5abd9e52
Vedar <islamparchev006@gmail.com>
2023-08-08 15:59:32 +0300

    refactor-accordion

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -2,12 +2,12 @@ import React,{useState} from 'react'
 
 import './Accordion.scss';
 
-const Accordion = () => {
+const Accordion = ({children,title}:any) => {
 	const [open,setOpen] = useState<boolean>(false);
 	return (
 		<div className={`Accordion ${open && 'active'}`}>
 			<div className="Accordion__header" onClick={()=> setOpen(!open)}>
-				<span>Новинки</span> 
+				<span>{title}</span> 
 				<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
 					<path fill-rule="evenodd" clip-rule="evenodd" d="M16.7072 7.70725L10.7073 13.7073C10.3163 
 					14.0983 9.68425 14.0983 9.29325 13.7073L3.29325 7.70725C2.90225 7.31625 2.90225 6.68425 
@@ -17,29 +17,7 @@ const Accordion = () => {
 				</svg>
 			</div>
 			<div className="Accordion__body">
-				<ul className="list-reset Accordion__items">
-					<li className="Accordion__item">
-						<a href="/">
-						Складной нож
-						</a>
-					</li>
-					<li className="Accordion__item">
-						<a href="/">
-						Складной нож
-						</a>
-					</li>
-					<li className="Accordion__item">
-						<a href="/">
-						Складной нож
-						</a>
-					</li>
-					<li className="Accordion__item">
-						<a href="/">
-						Складной нож
-						</a>
-					</li>
-					
-				</ul>
+				{children}
 			</div>
 		</div>
 	)
--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -8,10 +8,106 @@ import styles from './Filter.module.scss';
 const Filter:FC = () => {
 	return (
 		<div className={styles.Filter}>
-			<Accordion/>
-			<Accordion/>
-			<Accordion/>
-			<Accordion/>
+			<Accordion title="Новинки">
+				<ul className="list-reset Accordion__items">
+					<li className="Accordion__item">
+						<a href="/">
+						Складной нож
+						</a>
+					</li>
+					<li className="Accordion__item">
+						<a href="/">
+						Складной нож
+						</a>
+					</li>
+					<li className="Accordion__item">
+						<a href="/">
+						Складной нож
+						</a>
+					</li>
+					<li className="Accordion__item">
+						<a href="/">
+						Складной нож
+						</a>
+					</li>
+					
+				</ul>
+			</Accordion>
+			<Accordion title="Новинки">
+				<ul className="list-reset Accordion__items">
+					<li className="Accordion__item">
+						<a href="/">
+						Складной нож
+						</a>
+					</li>
+					<li className="Accordion__item">
+						<a href="/">
+						Складной нож
+						</a>
+					</li>
+					<li className="Accordion__item">
+						<a href="/">
+						Складной нож
+						</a>
+					</li>
+					<li className="Accordion__item">
+						<a href="/">
+						Складной нож
+						</a>
+					</li>
+					
+				</ul>
+			</Accordion>
+			<Accordion title="Новинки">
+				<ul className="list-reset Accordion__items">
+					<li className="Accordion__item">
+						<a href="/">
+						Складной нож
+						</a>
+					</li>
+					<li className="Accordion__item">
+						<a href="/">
+						Складной нож
+						</a>
+					</li>
+					<li className="Accordion__item">
+						<a href="/">
+						Складной нож
+						</a>
+					</li>
+					<li className="Accordion__item">
+						<a href="/">
+						Складной нож
+						</a>
+					</li>
+					
+				</ul>
+			</Accordion>
+			<Accordion title="Новинки">
+				<ul className="list-reset Accordion__items">
+					<li className="Accordion__item">
+						<a href="/">
+						Складной нож
+						</a>
+					</li>
+					<li className="Accordion__item">
+						<a href="/">
+						Складной нож
+						</a>
+					</li>
+					<li className="Accordion__item">
+						<a href="/">
+						Складной нож
+						</a>
+					</li>
+					<li className="Accordion__item">
+						<a href="/">
+						Складной нож
+						</a>
+					</li>
+					
+				</ul>
+			</Accordion>
 		</div>
 	)
 }
